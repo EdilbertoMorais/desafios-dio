eecb1364c64d0123356e6bf8b48dcc462b629a84
Edilberto Morais <edilbertocmorais@gmail.com>
2023-10-18 19:37:27 -0300

    implementação das camadas de dominio, negocio e tratativas de erros. Implementação do Railway/PostgreSQL

---
+++ santander-dev-week-2023/src/main/java/me/dio/domain/model/Feature.java
@@ -0,0 +1,14 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Entity;
+
+/**
+ * Classe extende a BaseItem, pois possui a mesma implementação de código da classe News
+ *
+ * @author Edilberto Morais
+ */
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
